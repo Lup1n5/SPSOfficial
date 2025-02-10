96594887f14b8ff5c60f179775654e0edca50b99
Lup1n5 <146459441+Lup1n5@users.noreply.github.com>
2025-02-09 21:14:34 -0500

    Update main.js with some new code including.....
    - some tablist functionality to track who is on
    - new user system so inf users with no upd

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.1.3/firebase-app.js';
 import { getDatabase, ref, set, onValue, get, DataSnapshot } from 'https://www.gstatic.com/firebasejs/9.1.3/firebase-database.js';
 import { getAuth, 
@@ -8,11 +7,6 @@ import { getAuth,
          signInWithPopup,
          onAuthStateChanged,
          signOut } from 'https://www.gstatic.com/firebasejs/9.1.3/firebase-auth.js';
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-//hi
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAYjLbsdGgVccTHa_bpEaDh7orYmzldiMk",
   authDomain: "stewflandic-permission-system.firebaseapp.com",
@@ -29,44 +23,49 @@ const auth = getAuth();
 const provider = new GoogleAuthProvider();
 const db = getDatabase(app);
 
-// logged in and logged out sections
 const loggedInView = document.getElementById('logged-in-view')
 const loggedOutView = document.getElementById('logged-out-view')
 const userEmail = document.getElementById('user-email')
-
-// email and password for signin
 const emailSignInForm = document.getElementById('signin-email-input')
 const passwordSignInForm = document.getElementById('signin-password-input')
-
-// email and password for signup
 const emailSignUpForm = document.getElementById('signup-email-input')
 const passwordSignUpForm = document.getElementById('signup-password-input')
-
-// Buttons
-const signInGoogleBtn = document.getElementById('sign-in-with-google-btn')
-const signUpGoogleBtn = document.getElementById('sign-up-with-google-btn')
-//const googleBtns = [signInGoogleBtn, signUpGoogleBtn]
-
-//const createAccountBtn = document.getElementById('sign-up-btn')
 const loginBtn = document.getElementById('sign-in-btn')
 const logoutBtn = document.getElementById('logout-button')
-
-//const chatHeader = document.querySelector('.chat-header')
 const chatMessages = document.querySelector('.chat-messages')
 const chatInputForm = document.querySelector('.chat-input-form')
 const chatInput = document.querySelector('.chat-input')
 const sendBtn = document.querySelector(".send-button")
 var messageSender = ''
 var email = ""
-
-
 let uid = '';
-// Detects state change
-
+function logout() {
+  const refage = ref(db, `users/${uid}`)
+      set(refage, null)
+  for (var i = 0; i<chatMessages.childElementCount; i++) { 
+    chatMessages.removeChild(chatMessages.firstChild); 
+    signOut(auth).then(() => {
+      // Sign-out successful.
+    }).catch((error) => {
+      // An error happened.
+    });
+}
+  
+}
+var inactivityTime = function () {
+  var time;
+  window.onload = resetTimer;
+  document.onmousemove = resetTimer;
+  document.onkeydown = resetTimer;
+  
+  function resetTimer() {
+      clearTimeout(time);
+      time = setTimeout(logout(), 300000)
+      // 1000 milliseconds = 1 second
+  }
+};
 onAuthStateChanged(auth, (user) => {
     if (user) {
-      // User is signed in, see docs for a list of available properties
-      // https://firebase.google.com/docs/reference/js/auth.user
       uid = user.uid;
       email = user.email
       //console.log(email)
@@ -76,7 +75,6 @@ onAuthStateChanged(auth, (user) => {
       passwordSignInForm.value = ""
       loggedOutView.style.display = 'none'
       messageSender = email
-      //console.log(messageSender);
       const refage = ref(db, `users/${uid}`)
       let timestamp = new Date().toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })
   let message = {
@@ -84,41 +82,16 @@ onAuthStateChanged(auth, (user) => {
     text: `${messageSender} has connected.`,
     timestamp,
   }
-
-  
-  //console.log(message)
   const messageRef = ref(db,`messages/${uid}`)
   set(messageRef,message)
-
       set(refage, email)
-      // ...
     } else {
       // User is signed out
-      // ...
-      
       loggedInView.style.display = 'none' 
       loggedOutView.style.display = 'block'
        
     }
   });
-  // function closeIt()
-  // {
-  //   for (var i = 0; i<chatMessages.childElementCount; i++) { 
-  //     chatMessages.removeChild(chatMessages.firstChild); 
-  // }
-  //   signOut(auth).then(() => {
-  //     // Sign-out successful.
-  //   }).catch((error) => {
-  //     // An error happened.
-  //   });
-  // }
-  // window.onbeforeunload = closeIt;
-
-// Event Listeners for Buttons
-// Click on Create Account Button
-
-
-// Click on Login Button
 loginBtn.addEventListener('click', () => {
     signInWithEmailAndPassword(auth, emailSignInForm.value, passwordSignInForm.value)
         .then((userCredential) => {
@@ -133,67 +106,29 @@ loginBtn.addEventListener('click', () => {
             //console.log(errorMessage)
             passwordSignInForm.value = ""
         });
-
-    //console.log('Login Clicked')
-    //console.log(`Email: ${emailSignInForm.value}`)
-    ////console.log(`Password: ${passwordSignInForm.value}`)
   })
 passwordSignInForm.addEventListener("keypress", function(event) {
-  // If the user presses the "Enter" key on the keyboard
   if (event.key === "Enter") {
-    // Cancel the default action, if needed
     event.preventDefault();
-    // Trigger the button element with a click
     loginBtn.click();
   }
 });
 
-
-
-// logout button
-logoutBtn.addEventListener('click', () => {
-  // const refage = ref(db, `users/${uid}`)
-  // set(refage, null)
-  // const messageRef = ref(db,`messages/${uid}`)
-  // set(messageRef,null)
-  for (var i = 0; i<chatMessages.childElementCount; i++) { 
-    chatMessages.removeChild(chatMessages.firstChild); 
-}
-    signOut(auth).then(() => {
-        // Sign-out successful.
-      }).catch((error) => {
-        // An error happened.
-      });
-      
-    //console.log('Logout Clicked')
-})
+logoutBtn.addEventListener('click', () => {logout()})
 const createChatMessageElement = (message) => {
   const newMessage = document.createElement("div");
   let timestamp = new Date().toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })
-  // let string = ((timestamp - ":") - "PM") - "AM"
   let time1 = timestamp.replace(/[:APM]/g, ""); 
   let time2 = message.timestamp.replace(/[:APM]/g, ""); 
-  //console.log(Number(time1))
-  //console.log(Number(time2))
-  //console.log(Number(time2)-Number(time1))
   if (Math.abs(Number(time2)-Number(time1)) <2) {
-   
-// Add some text content to the Message
 newMessage.innerHTML = `<div class="message ${message.sender === messageSender ? 'blue-bg' : 'gray-bg'}">
-    <div class="message-sender">${message.timestamp}:          ${message.sender}</div>
-    <div class="message-text">${message.text}</div>
-    
+  <div class="message-sender">${message.timestamp}:          ${message.sender}</div>
+  <div class="message-text">${message.text}</div>
   </div>`;
-
-// Append the Message to an existing element in the DOM
 chatMessages.appendChild(newMessage);
 chatMessages.scrollTop = chatMessages.scrollHeight
   }
 }
-
-
-
-
 sendBtn.addEventListener('click', () => {
   let timestamp = new Date().toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })
   let message = {
@@ -202,176 +137,46 @@ sendBtn.addEventListener('click', () => {
     timestamp,
   }
   if (message.text) {
-  //console.log(message)
   const messageRef = ref(db,`messages/${uid}`)
   set(messageRef,message)
   const counterRef = ref(db,'messageCount')
   get(counterRef).then((DataSnapshot) => {
-    //console.log(DataSnapshot.val())
     set(counterRef,DataSnapshot.val()+1)
-  
-
   } )
-  
-  
-  //console.log(counterRef)
   createChatMessageElement(message);  
-  
-
-  ///Send message through firebase
-
-
-  //Clear input field
   chatInput.value = ""
-
- // Scroll to bottom of chat messages
-  //chatMessages.scrollTop = chatMessages.scrollHeight
   }
 })
 chatInput.addEventListener("keypress", function(event) {
-  // If the user presses the "Enter" key on the keyboard
   if (event.key === "Enter") {
-    // Cancel the default action, if needed
     event.preventDefault();
-    // Trigger the button element with a click
     sendBtn.click();
   }
 });
-const allusers = ref(db, "users")
-var userList = [];
-
-get(allusers).then((snapshot) =>{
-  let snap = snapshot.val()
-  userList = Object.keys(snap)
-  
-  //console.log(userList)
-  //console.log(userList.at(3))
-  
-
-
-const user1ref = ref(db, `messages/${userList.at(0)}/text`)
-const user2ref = ref(db, `messages/${userList.at(1)}/text`)
-const user3ref = ref(db, `messages/${userList.at(2)}/text`)
-const user4ref = ref(db, `messages/${userList.at(3)}/text`)
-const user5ref = ref(db, `messages/${userList.at(4)}/text`)
-const user6ref = ref(db, `messages/${userList.at(5)}/text`)
-const user7ref = ref(db, `messages/${userList.at(6)}/text`)
-const user8ref = ref(db, `messages/${userList.at(7)}/text`)
-onValue(user1ref, () =>{
-  let reef = ref(db, `messages/${userList.at(0)}`)
+const allmessages = ref(db, "messages")
+get(allmessages).then((snapshot) =>{
+  Object.keys(snapshot.val()).forEach((poop) => {
+    const userRef = ref(db, `messages/${poop}/text`)
+    onValue(userRef, () =>{
+  const reef = ref(db, `messages/${poop}`)
   get(reef).then((snapshot) =>{
     let snap = snapshot.val()
-    //console.log(snap)
     if (snap.sender != messageSender) {
-      //,
-
-             
-    createChatMessageElement(snap)
-    }
-
-  })
-})
-onValue(user2ref, () =>{
-  let reef = ref(db, `messages/${userList.at(1)}`)
-  get(reef).then((snapshot) =>{
-    let snap = snapshot.val()
-    //console.log(snap)
-    if (snap.sender != messageSender) {
-      //,
-
-             
-    createChatMessageElement(snap)
-    }
-
-  })
-})
-onValue(user3ref, () =>{
-  let reef = ref(db, `messages/${userList.at(2)}`)
-  get(reef).then((snapshot) =>{
-    let snap = snapshot.val()
-    //console.log(snap)
-    if (snap.sender != messageSender) {
-      //,
-
-             //ear
-    createChatMessageElement(snap)
-    }
-
-  })
-})
-onValue(user4ref, () =>{
-  let reef = ref(db, `messages/${userList.at(3)}`)
-  get(reef).then((snapshot) =>{
-    let snap = snapshot.val()
-    //console.log(snap)
-    if (snap.sender != messageSender) {
-      //,
-             
-             
-    createChatMessageElement(snap)
-    }
-
-  })
-})
-onValue(user5ref, () =>{
-  let reef = ref(db, `messages/${userList.at(4)}`)
-  get(reef).then((snapshot) =>{
-    let snap = snapshot.val()
-    //console.log(snap)
-    if (snap.sender != messageSender) {
-      //,
-
-             
-             
-    createChatMessageElement(snap)
-    }
-
-  })
-})
-onValue(user6ref, () =>{
-  let reef = ref(db, `messages/${userList.at(5)}`)
-  get(reef).then((snapshot) =>{
-    let snap = snapshot.val()
-    //console.log(snap)
-    if (snap.sender != messageSender) {
-      //,
-
-             
-    createChatMessageElement(snap)
-    }
-
-  })
-})
-onValue(user7ref, () =>{
-  let reef = ref(db, `messages/${userList.at(6)}`)
-  get(reef).then((snapshot) =>{
-    let snap = snapshot.val()
-    //console.log(snap)
-    if (snap.sender != messageSender) {
-      //,
-
-             
-    createChatMessageElement(snap)
-    }
-
-  })
-})
-onValue(user8ref, () =>{
-  let reef = ref(db, `messages/${userList.at(7)}`)
-  get(reef).then((snapshot) =>{
-    let snap = snapshot.val()
-    //console.log(snap)
-    if (snap.sender != messageSender) {
-      //,
-
-             
     createChatMessageElement(snap)
     }
 
   })
 })
 
+  });
 })
+
+
+
+
+
+
+
 
 
 
